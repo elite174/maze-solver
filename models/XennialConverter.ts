b56f9bf3d537fb8fb58057d880b0fb6224f31dfb
Vladislav <lipatov.xex@gmail.com>
2022-01-01 13:47:23 +0800

    Applied compression

--- models/XennialConverter.ts
+++ models/XennialConverter.ts
@@ -19,6 +19,10 @@ export class XennialConverter {
 
   private currentLineNumber: number;
 
+  private currentDirection: Direction | null = null;
+
+  private sameDirectionCount = 0;
+
   constructor(
     initialLineNumber: number,
     initialPosition: Coordinates,
@@ -31,24 +35,50 @@ export class XennialConverter {
   convertPath(path: Coordinates[]): string[] {
     const instructions: string[] = [];
 
+    let direction: Direction | null = null;
+
     for (const position of path) {
       // i => row
       if (this.currentPosition.i - position.i === 1) {
-        instructions.push(this.getInstruction(Direction.Up));
+        direction = Direction.Up;
       } else if (this.currentPosition.i - position.i === -1) {
-        instructions.push(this.getInstruction(Direction.Down));
+        direction = Direction.Down;
       } // j => column
       else if (this.currentPosition.j - position.j === 1) {
-        instructions.push(this.getInstruction(Direction.Left));
+        direction = Direction.Left;
       } else if (this.currentPosition.j - position.j === -1) {
-        instructions.push(this.getInstruction(Direction.Right));
+        direction = Direction.Right;
       } else {
         throw new Error(
           `The difference is bigger than 1...: ${this.currentPosition}, ${position}`
         );
       }
 
+      if (direction === null) throw new Error("Direction is null");
+
+      if (this.currentDirection === null) {
+        this.currentDirection = direction;
+        this.sameDirectionCount = 1;
+      } else {
+        if (direction !== this.currentDirection) {
+          instructions.push(
+            this.getInstruction(this.currentDirection, this.sameDirectionCount)
+          );
+
+          this.currentDirection = direction;
+          this.sameDirectionCount = 1;
+        } else {
+          this.sameDirectionCount += 1;
+        }
+      }
+
       this.currentPosition = position;
+    }
+
+    if (this.sameDirectionCount > 0) {
+      instructions.push(
+        this.getInstruction(this.currentDirection, this.sameDirectionCount)
+      );
     }
 
     console.log("Total instructions: ", instructions.length);
@@ -56,9 +86,16 @@ export class XennialConverter {
     return instructions;
   }
 
-  private getInstruction(direction: Direction) {
-    return `${this.currentLineNumber++} POKE 0,${direction}${
-      this.withComments ? `: REM ${directionToString[direction]}` : ""
-    }`;
+  private getInstruction(direction: Direction | null, stepsCount: number) {
+    if (direction === null) throw new Error("current direction is null");
+
+    return stepsCount === 1
+      ? `${this.currentLineNumber++} POKE 0,${direction}${
+          this.withComments ? `: REM ${directionToString[direction]}` : ""
+        }`
+      : `${this
+          .currentLineNumber++} FOR X = 1 to ${stepsCount}: POKE 0,${direction}: NEXT X${
+          this.withComments ? `: REM ${directionToString[direction]}` : ""
+        }`;
   }
 }
